6066c9c2705f9f9e45d3be3fdcab174a594cdcda
nghe0001 <nghe0001@algonquinlive.com>
2019-03-25 03:02:32 -0400

    kevin article

---
+++ app/src/main/java/com/cst2335/kevin/NytArticle.java
@@ -0,0 +1,45 @@
+package com.cst2335.kevin;
+
+public class NytArticle {
+
+    private String title;
+    private String body;
+    private int articleID;
+
+
+    public NytArticle(String title, String body, int articleID){
+        this.title = title;
+        this.body = body;
+        this.articleID = articleID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public int getNewsID() {
+        return articleID;
+    }
+
+    public void setNewsID(int newsID) {
+        this.articleID = newsID;
+    }
+}
+
+
+
+
+
+
