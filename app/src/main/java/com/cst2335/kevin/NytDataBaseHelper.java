1d87e1afdbd747bf028f940c5d1a17395a0e85ee
nghe0001 <nghe0001@algonquinlive.com>
2019-04-08 01:50:35 -0400

    Milestone3

---
+++ app/src/main/java/com/cst2335/kevin/NytDataBaseHelper.java
@@ -0,0 +1,63 @@
+package com.cst2335.kevin;
+
+import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class NytDataBaseHelper extends SQLiteOpenHelper {
+
+    /** database */
+    private SQLiteDatabase database;
+    /** database table name */
+    public static final String DATABASE_NAME = "NytDB";
+    /** database version number*/
+    public static final int VERSION_NUM = 1;
+    /** database table name*/
+    public static final String TABLE_NAME = "Nyt_Table";
+    /** database table column names */
+    public static final String COL_ID = "id";
+    public static final String COL_Article = "article";
+
+
+
+    public NytDataBaseHelper(Activity ctx){
+        super(ctx, DATABASE_NAME,null,VERSION_NUM);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+        Log.i("Database onCreate:", "trying to create the database .");
+        //Make sure you put spaces between SQL statements and Java strings:
+        String sql = "CREATE TABLE " + TABLE_NAME + "( "
+                + COL_ID +" INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COL_Article + " TEXT)";
+        Log.i("Database onCreate:", "the query is: " + sql);
+        db.execSQL(sql);
+        // log messages
+        Log.i("Database onCreate:", "the database is created.");
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        Log.i("Database upgrade:", " Old version:" + oldVersion + " newVersion:"+newVersion);
+
+        //Delete the old table:
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+
+        //Create a new table:
+        onCreate(db);
+    }
+
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        Log.i("Database downgrade", " Old version:" + oldVersion + " newVersion:"+newVersion);
+
+        //Delete the old table:
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+
+        //Create a new table:
+        onCreate(db);
+    }
+}
